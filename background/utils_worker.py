e20e98da80bbbe95b5e71a444124e767cb093aee
Vlad Yashenkov <yashenkov.v@yandex.ru>
2026-08-27 16:35:03 +0700

    fix: add bg task for generate_thread_memories_by_color

--- background/utils_worker.py
+++ background/utils_worker.py
@@ -1,7 +1,7 @@
 from .tasks import (generate_thread_memory,
                     generate_translated_user_information,
                     translate_user_information_by_thread_id,
-                    generate_thread_momories_by_color_level)
+                    generate_thread_momory_by_id)
 from background.base import (redis_settings,
                              _redis_pool,
                              get_redis_background_pool)
@@ -42,7 +42,7 @@ class WorkerSettings:
         generate_thread_memory,
         generate_translated_user_information,
         translate_user_information_by_thread_id,
-        generate_thread_momories_by_color_level,
+        generate_thread_momory_by_id,
         # try_start_stop_vision_profile_by_account_id,
         # try_block_thread_by_account_id,
         # parse_thread,
